000cd0eb241db89a4a1b4a8aa35c477bebd62c7e
urso <steffen.siering@elastic.co>
2017-08-30 14:55:39 +0200

    Add some godocs

--- beater/pub.go
+++ beater/pub.go
@@ -7,6 +7,10 @@ import (
 	"github.com/elastic/beats/libbeat/beat"
 )
 
+// publisher forwards batches of events to libbeat. It uses GuaranteedSend
+// to enable infinite retry of events being processed.
+// If the publisher it's input is channel is full, an error is returned
+// immediately.
 type publisher struct {
 	events chan []beat.Event
 	client beat.Client
@@ -17,6 +21,9 @@ var (
 	errFull = errors.New("Queue is full")
 )
 
+// newPublisher creates a new publisher instance. A new go-routine is started
+// for forwarding events to libbeat. Stop must be called to close the
+// beat.Client and free resources.
 func newPublisher(pipeline beat.Pipeline) (*publisher, error) {
 	client, err := pipeline.ConnectWith(beat.ClientConfig{
 		PublishMode: beat.GuaranteedSend,
@@ -39,12 +46,18 @@ func newPublisher(pipeline beat.Pipeline) (*publisher, error) {
 	return p, nil
 }
 
+// Stop closes all channels and waits for the the worker to stop.
+// The worker will drain the queue on shutdown, but no more events
+// will be published.
 func (p *publisher) Stop() {
 	close(p.events)
 	p.client.Close()
 	p.wg.Wait()
 }
 
+// Send tries to forward events to the publishers worker. If the queue is full,
+// an error is returned.
+// Calling send after Stop will cause a panic.
 func (p *publisher) Send(batch []beat.Event) error {
 	select {
 	case p.events <- batch:
